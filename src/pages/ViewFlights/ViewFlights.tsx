4bb13aad62eadc170db4e8898fa349ff36861e32
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2024-11-17 18:15:47 -0500

    fix: se quitó el header

--- src/pages/ViewFlights/ViewFlights.tsx
+++ src/pages/ViewFlights/ViewFlights.tsx
@@ -9,9 +9,6 @@ const ViewFlights = () => {
     <>
       <Asidebar />
       <LayoutPrincipal>
-        <header>
-          <h2 className='text-indigo-600 ml-5 font-medium text-2xl'>Mis tiquetes</h2>
-        </header>
         <section className='w-full grid grid-cols-1 sm:grid-cols-4 mt-6'>
           {Array(5).fill(0).map((_, i) => (
             <Flight key={i} />
