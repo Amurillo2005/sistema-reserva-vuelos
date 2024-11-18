d36d110795f91a0933e82a3136f87aec81c3936b
Andres Murillo <murilloandrescaleb@gmail.com>
2024-11-18 11:43:30 -0500

    Mejoras en los formularios

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import "./Login.css"
+
 
 export const Login = () => {
 
@@ -22,23 +24,18 @@ export const Login = () => {
 
     return (
         <>
-            <div className='text-white h-[100vh] flex justify-center items-center bg-cover' style={{ "backgroundImage": "url(../public/avion.jpg)" }}>
-                <div>
-                    <div className='bg-slate-800 border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative'>
-                        <h1 className='text-4xl text-white font-bold text-center mb-6'>Iniciar sesión</h1>
-                        <form action="" onSubmit={handleSubmit}>
-                            <div className='relative my-10'>
-                                <input type="text" name="usuario" value={formData.usuario} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su usuario' />
-                                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Usuario</label>
-                            </div>
-                            <div className='relative my-10'>
-                                <input type="password" className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su contraseña' />
-                                <label htmlFor="" name="contraseña" value={formData.contraseña} onChange={handleChange} className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Contraseña</label>
-                            </div>
-                            <button type='Submit' className='w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white py-2 transition-colors duration-300 '>Iniciar sesión</button>
-                        </form>
+            <div className="wrapper">
+                <form action="" onSubmit={handleSubmit}>
+                    <h2>Iniciar sesión</h2>
+                    <div className="input-box">
+                        <input type="text" name="usuario" value={formData.usuario} placeholder="Usuario" onChange={handleChange}/>
                     </div>
-                </div>
+                    <div className="input-box">
+                        <input type="password" name="contraseña" value={formData.contraseña} placeholder="Contraseña" onChange={handleChange}/>
+                    </div>
+                    
+                    <button type="submit">Iniciar sesión</button>
+                </form>
             </div>
         </>
     )
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -26,43 +26,30 @@ export const Register = () => {
   }
   return (
     <>
-      <div className='text-white h-[100vh] flex justify-center items-center bg-cover' style={{ "backgroundImage": "url(../public/avion.jpg)" }}>
-        <div>
-          <div className='bg-slate-800 border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative'>
-            <h1 className='text-4xl text-white font-bold text-center mb-6'>Registrarse</h1>
-            <form onSubmit={handleSubmit}>
-              <div className='relative my-10'>
-                <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su nombre' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Nombre</label>
-              </div>
-              <div className='relative my-10'>
-                <input type="text" name="apellido" value={formData.apellido} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su apellido' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Apellido</label>
-              </div>
-              <div className='relative my-10'>
-                <input type="text" name="usuario" value={formData.usuario} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su usuario' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Usuario</label>
-              </div>
-              <div className='relative my-10'>
-                <input type="number" name="identificacion" value={formData.identificacion} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su identificación' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Identificación</label>
-              </div>
-              <div className='relative my-10'>
-                <input type="email" name="email" value={formData.email} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su email' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Email</label>
-              </div>
-              <div className='relative my-10'>
-                <input type="password" name="contraseña" value={formData.contraseña} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su contraseña' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Contraseña</label>
-              </div>
-              <div className='relative my-10'>
-                <input type="number" name="telefono" value={formData.telefono} onChange={handleChange} className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder='Escriba su telefono' />
-                <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 peer-focus:top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Teléfono</label>
-              </div>
-              <button type='Submit' className='w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white py-2 transition-colors duration-300 '>Registrarse</button>
-            </form>
+      <div className="wrapper">
+        <form action="" onSubmit={handleSubmit}>
+          <h2>Registrarse</h2>
+          <div className="input-box">
+            <input type="text" name="nombre" value={formData.nombre} placeholder="Nombre" onChange={handleChange} />
           </div>
-        </div>
+          <div className="input-box">
+            <input type="text" name="apellido" value={formData.apellido} placeholder="Apellido" onChange={handleChange} />
+          </div>
+          <div className="input-box">
+            <input type="text" name="usuario" value={formData.usuario} placeholder="Usuario" onChange={handleChange} />
+          </div>
+          <div className="input-box">
+            <input type="number" name="identificacion" value={formData.identificacion} placeholder="Identificación" onChange={handleChange} />
+          </div>
+          <div className="input-box">
+            <input type="email" name="email" value={formData.email} placeholder="Correo" onChange={handleChange} />
+          </div>
+          <div className="input-box">
+            <input type="password" name="contraseña" value={formData.contraseña} placeholder="Contraseña" onChange={handleChange} />
+          </div>
+
+          <button type="submit">Registrarse</button>
+        </form>
       </div>
     </>
   )
